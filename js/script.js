28c5ff383df03d8ec952864920dd24410910764a
yerimen26 <yerigyt26@gmail.com>
2023-06-27 22:01:28 -0300

    Agregamos editar tarea

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
 let tareas = [];
 
+//este evento verifica que este todo cargado
+document.addEventListener("DOMContentLoaded", () => {
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    const inputId = document.querySelector("#id");
     const inputText = document.querySelector("#text");
 
-    tareas.push({
+    if(inputId.value){
+        tareas.forEach((tarea) => {
+            if(tarea.id == inputId.value){
+                tarea.text = inputText.value;
+            }
+        })
+    } else{
+       tareas.push({
         id: Date.now(),
         text: inputText.value,
         complete: false,
-    });
-
+    }); 
+    }
+    
+    inputId.value = "";
     // inputText.value = "";
     event.target.reset();
     localStorage.setItem("tareas", JSON.stringify(tareas));
 
     renderTareas();
 });
+    renderTareas();
+});
+
 
 const renderTareas = () => {
     tareas = JSON.parse(localStorage.getItem("tareas")) || [];
@@ -31,7 +46,7 @@ const renderTareas = () => {
     <td class="${tarea.complete ? "complete" : ""}">${tarea.text}</td>
     <td>
         <button type="button" data-id="${tarea.id}" class="btn-complete">Completar</button>
-        <button type="button">Editar</button>
+        <button type="button" onclick="editarTarea(${tarea.id})">Editar</button>
         <button type="button" onclick="borrarTarea(${tarea.id})">Borrar</button>
     </td>
 </tr>
@@ -57,6 +72,18 @@ const completarTarea = (id) => {
     renderTareas();
 };
 
+const editarTarea = (id) => {
+    const tarea = tareas.find(tarea => tarea.id == id);
+
+    if(tarea){
+        const inputId = document.querySelector("#id");
+        inputId.value = tarea.id
+        const inputText = document.querySelector("#text");
+        inputText.value = tarea.text;
+
+    }
+}
+
 const borrarTarea = (id) => {
     if(confirm('Estas seguro?')){
         const filtradas = tareas.filter(tarea => tarea.id != id)
@@ -67,8 +94,4 @@ const borrarTarea = (id) => {
 } 
 
 
-//este evento verifica que este todo cargado
-document.addEventListener("DOMContentLoaded", () => {
-    renderTareas();
-});
 
